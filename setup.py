de7b65d00eaaf352b2ff0025ed58ae01161be768
Michael Hirsch <scienceopen@users.noreply.github.com>
2015-10-09 11:59:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup 
 
-with open('README.rst') as f:
+with open('README.rst','r') as f:
 	long_description = f.read()
 	
 setup(name='gridaurora',
@@ -10,7 +10,6 @@ setup(name='gridaurora',
 	  description='utilities for ionospheric gridding, particularly for the aurora',
 	  long_description=long_description,
 	  author='Michael Hirsch',
-	  author_email='hirsch617@gmail.com',
 	  url='https://github.com/scienceopen/gridaurora',
 	  install_requires=['lowtran','histutils','pandas','six','pytz','numpy','scipy','h5py'],
           dependency_links = ['https://github.com/scienceopen/lowtran/tarball/master#egg=lowtran',
