4ea0055817d5b0c4d6b363a4965018dd3b390a0e
scivision <scivision@users.noreply.github.com>
2015-10-25 16:29:50 -0400

    write all eigenprofiles to hdf5

--- gridaurora/writeeigen.py
+++ gridaurora/writeeigen.py
@@ -1,28 +1,38 @@
+from __future__ import division,absolute_import
 from six import string_types
 import h5py
 from os.path import expanduser
 from datetime import datetime
 from pytz import UTC
+#
+from histutils.fortrandates import forceutc
 
 epoch = datetime(1970,1,1,tzinfo=UTC)
 
 def writeeigen(fn,ver,prates,lrates,tez,latlon):
-    assert isinstance(fn,string_types)
+    if not isinstance(fn,string_types):
+        return
 
     if fn.endswith('.h5'):
-        h5fn = expanduser(fn)
+        fn = expanduser(fn)
         print('writing to '+ fn)
-        ut1_unix = [(t-epoch).total_seconds() for t in ver.items.to_pydatetime()]
-        with h5py.File(h5fn,'w',libver='latest') as f:
+
+        ut1_unix = [(forceutc(t)-epoch).total_seconds() for t in ver.labels.to_pydatetime()]
+
+        with h5py.File(fn,'w',libver='latest') as f:
+            bdt = h5py.special_dtype(vlen=bytes)
             f['/sensorloc'] = latlon
             #VER
-            d=f.create_dataset('/eigenprofile',data=ver.values,compression='gzip')
+            d=f.create_dataset('/ver/eigenprofile',data=ver.values,compression='gzip')
             d=f.create_dataset('/altitude',data=ver.major_axis)
-            d=f.create_dataset('/Ebins',data=ver.minor_axis)
+            d=f.create_dataset('/Ebins',data=ver.items)
             d=f.create_dataset('/ut1_unix',data=ut1_unix) #float
-            #prod/loss
-            f['/state'] = prates[0].columns.tolist()
-            d=f.create_dataset('/production',data=prates.values,compression='gzip')
-            d=f.create_dataset('/loss',data=lrates.values,compression='gzip')
+            d=f.create_dataset('/ver/wavelength',data=ver.minor_axis)
+            #prod
+            d=f.create_dataset('/prod/eigenprofile',data=prates.values,compression='gzip')
+            d=f.create_dataset('/prod/reaction',data=prates.minor_axis,dtype=bdt)
+            #loss
+            d=f.create_dataset('/loss/eigenprofiles',data=lrates.values,compression='gzip')
+            d=f.create_dataset('/loss/reaction',data=lrates.minor_axis,dtype=bdt)
             #energy deposition
-            d=f.create_dataset('/energydeposition',data=tez.value,compression='gzip')
+            d=f.create_dataset('/energydeposition',data=tez.values,compression='gzip')
