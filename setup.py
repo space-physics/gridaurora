54b028f815ccd53613de6b633b334a1463e98fdf
michael <scienceopen@users.noreply.github.com>
2015-11-10 01:02:37 -0500

    data install path

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ setup(name='gridaurora',
 	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/gridaurora',
+    data_files=[('gridaurora/data',['gridaurora/data/RecentIndices.txt'])],
 	  install_requires=['histutils',
                         'pathlib2>=2.1.0'],
-      extras_require={'lowtran':'lowtran'}, #optional
-          dependency_links = ['https://github.com/scienceopen/lowtran/tarball/master#egg=lowtran',
-                             'https://github.com/scienceopen/histutils/tarball/master#egg=histutils'],
-          packages=['gridaurora'],
-          package_data={'gridaurora.precompute': ['*.h5']},
+   extras_require={'lowtran':'lowtran'}, #optional
+   dependency_links = ['https://github.com/scienceopen/lowtran/tarball/master#egg=lowtran',
+                     'https://github.com/scienceopen/histutils/tarball/master#egg=histutils'],
+   packages=['gridaurora'],
+   package_data={'gridaurora.precompute': ['*.h5']},
 	  )
 
 
