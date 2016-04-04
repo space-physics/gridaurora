9343b3f80822a9417c7f91e1c61b228b87e76029
scivision <scivision@users.noreply.github.com>
2016-04-04 03:38:37 -0400

    float

--- gridaurora/worldgrid.py
+++ gridaurora/worldgrid.py
@@ -1,7 +1,7 @@
 from numpy import arange,meshgrid
 
 def latlonworldgrid(latstep=5,lonstep=5):
-    lat = arange(-90,90+latstep,latstep)
-    lon = arange(-180,180+lonstep,lonstep)
+    lat = arange(-90.,90+latstep,latstep)
+    lon = arange(-180.,180+lonstep,lonstep)
     glon,glat = meshgrid(lon,lat)
     return glat,glon
