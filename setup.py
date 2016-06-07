9d5d1781276a22056ed8c6993e70d1e909364a65
scienceopen <scienceopen@users.noreply.github.com>
2016-06-06 21:15:25 -0400

    setup syntax

--- setup.py
+++ setup.py
@@ -13,14 +13,16 @@ with open('README.rst','r') as f:
 	long_description = f.read()
 
 setup(name='gridaurora',
+      version='0',
+      packages=['gridaurora'],
 	  description='utilities for ionospheric gridding, particularly for the aurora',
 	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/gridaurora',
-    data_files=[('gridaurora/data',['gridaurora/data/RecentIndices.txt'])],
+      data_files=[('gridaurora/data',['gridaurora/data/RecentIndices.txt'])],
 	  install_requires=['histutils'],
-   extras_require={'lowtran':'lowtran'}, #optional
-   dependency_links = ['https://github.com/scienceopen/lowtran/tarball/master#egg=lowtran',
+      extras_require={'lowtran':'lowtran'}, #optional
+      dependency_links = ['https://github.com/scienceopen/lowtran/tarball/master#egg=lowtran',
                      'https://github.com/scienceopen/histutils/tarball/master#egg=histutils'],
-   package_data={'gridaurora.precompute': ['*.h5']},
+      package_data={'gridaurora.precompute': ['*.h5']},
 	  )
