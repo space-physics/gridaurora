87d264d834847602c05c2d1e091e473a2ccaea88
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-24 00:54:32 -0400

    prereq

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python
-from setuptools import setup
-
-req = ['python-dateutil','pytz','nose','numpy','scipy','xarray','h5py','astropy','matplotlib','seaborn',
-      'sciencedates',
+req = ['python-dateutil','pytz','nose','numpy','scipy','xarray','h5py','astropy','matplotlib','seaborn',]
+pipreq=['sciencedates','lowtran',
         'pathvalidate']
-
+# %%
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except Exception as e:    
+    pip.main(['install',*req])
+pip.main(['install',*pipreq])
+# %%
+from setuptools import setup
 
 setup(name='gridaurora',
       packages=['gridaurora'],
       author='Michael Hirsch, Ph.D.',
       description='Gridding for auroral and ionospheric modeling',
       url='https://github.com/scivision/gridaurora',
-      version='1.0.1',
+      version='1.0.2',
       package_data={'gridaurora':['data/RecentIndices.txt'],
                     'gridaurora.precompute': ['*.h5']},
-	  install_requires=req,
-      extras_require={'lowtran':'lowtran'}, #optional
       classifiers=[
           'Intended Audience :: Science/Research',
           'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
           'Topic :: Scientific/Engineering :: Atmospheric Science',
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
           ],
 	  )
 
