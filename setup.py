e1f725ef3b201ecdf10e5000e0c1abb42ec9cc16
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-09 05:40:28 -0500

    v1.1.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='gridaurora',
       author='Michael Hirsch, Ph.D.',
       description='Gridding for auroral and ionospheric modeling',
       url='https://github.com/scivision/gridaurora',
-      version='1.1.7',
+      version='1.1.8',
       package_data={'gridaurora':['data/RecentIndices.txt'],
                     'gridaurora.precompute': ['*.h5']},
       classifiers=[
